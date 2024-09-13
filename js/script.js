90930d2081baa2a2140921e5f3f014b36b1b36c5
AmareshSahoo950 <=>
2024-09-13 15:17:32 +0530

    version-2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-const header = document.querySelector('.header')
+const header = document.querySelector(".header");
 header.innerHTML = `   <header class="bg-warning py-2">
     <div class="container">
-        <div class="d-flex justify-content-between">
-            <div class="contact-info">
+        <div class="d-flex justify-content-between p-2 flex-wrap contact-info">
                 <p class="mb-0"><i class="bi bi-telephone"></i> + (111) 1000-500-800</p>
                 <p class="mb-0">Mon - Sat : 9:00 - 18:00</p>
-            </div>
         </div>
     </div>
 </header>
@@ -37,18 +35,17 @@ header.innerHTML = `   <header class="bg-warning py-2">
     </div>
 </nav>
 
-`
+`;
 
+const footer = document.querySelector(".footer");
 
-const footer = document.querySelector('.footer')
-
-footer.innerHTML = ` <footer class="footer mt-auto py-5 bg-dark text-light "> <div class="container">
-    <div class="row">
+footer.innerHTML = ` <footer class="footer bg-dark text-light "> <div class="container">
+    <div class="row py-5">
       <!-- About Us -->
-      <div class="col-lg-3 col-md-6 mb-4">
+      <div class="col-lg-3 col-md-6 mb-6">
         <h5 class="text-warning">About Us</h5>
         <hr class="mb-2 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: orange; height: 2px;">
-        <p class="text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+        <p class="text-white">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
       </div>
 
       <!-- Information -->
@@ -56,11 +53,11 @@ footer.innerHTML = ` <footer class="footer mt-auto py-5 bg-dark text-light "> <d
         <h5 class="text-warning">Information</h5>
         <hr class="mb-2 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: orange; height: 2px;">
         <ul class="list-unstyled">
-          <li><a href="#" class="text-muted">About</a></li>
-          <li><a href="#" class="text-muted">Meet Our Team</a></li>
-          <li><a href="#" class="text-muted">Our Portfolio</a></li>
-          <li><a href="#" class="text-muted">Latest News</a></li>
-          <li><a href="#" class="text-muted">Contact</a></li>
+          <li><a href="#" class="text-white">About</a></li>
+          <li><a href="#" class="text-white">Meet Our Team</a></li>
+          <li><a href="#" class="text-white">Our Portfolio</a></li>
+          <li><a href="#" class="text-white">Latest News</a></li>
+          <li><a href="#" class="text-white">Contact</a></li>
         </ul>
       </div>
 
@@ -69,21 +66,15 @@ footer.innerHTML = ` <footer class="footer mt-auto py-5 bg-dark text-light "> <d
         <h5 class="text-warning">Helpful</h5>
         <hr class="mb-2 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: orange; height: 2px;">
         <ul class="list-unstyled">
-          <li><a href="#" class="text-muted"><i class="fa fa-phone"></i> +1 (817) *** 3377</a></li>
-          <li><a href="#" class="text-muted"><i class="fa fa-envelope"></i> info@example.com</a></li>
+          <li><a href="#" class="text-white"><i class="fa fa-phone"></i> +1 (817) *** 3377</a></li>
+          <li><a href="#" class="text-white"><i class="fa fa-envelope"></i> info@example.com</a></li>
         </ul>
-        <div class="social-icons">
-          <a href="#" class="text-light"><i class="fab fa-facebook"></i></a>
-          <a href="#" class="text-light"><i class="fab fa-twitter"></i></a>
-          <a href="#" class="text-light"><i class="fab fa-pinterest"></i></a>
-          <a href="#" class="text-light"><i class="fab fa-linkedin"></i></a>
-        </div>
       </div>
 
       <!-- Newsletter -->
       <div class="col-lg-3 col-md-6 mb-4">
         <h5 class="text-warning">Get the best viral stories straight into your inbox!</h5>
-        <hr class="mb-2 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: orange; height: 2px;">
+        <hr class="mb-2 mt-0 d-inline-block mx-auto" style="width: 100px; background-color: orange; height: 2px;">
         <form class="form-inline">
           <div class="input-group">
             <input type="email" class="form-control" placeholder="Email" aria-label="Email">
@@ -96,6 +87,6 @@ footer.innerHTML = ` <footer class="footer mt-auto py-5 bg-dark text-light "> <d
     </div>
   </div>
 
-  <div class="text-center py-3" style="background-color: #222;">
-    <p class="text-muted mb-0">&copy; Copyright 2024 by <a href="#" class="text-warning">Atiprayer.com</a></p>
-  </div></footer>`
+  <div class="text-center py-3 " style="background-color: #222;">
+    <p class="text-white mb-0">&copy; Copyright 2024 by <a href="#" class="text-warning">PriyaQubit.com</a></p>
+  </div></footer>`;
